fbb8d0e8338abae9a680e3fd4696220a59692263
noahm17 <noahm17@cs.washington.edu>
2026-05-06 18:50:59 -0700

    Game state is now updated in supabase

--- src/App.tsx
+++ src/App.tsx
@@ -22,23 +22,23 @@ function App() {
         if (!gameState) return;
 
         supabase
-        .from('games')
-        .update({ 
-            home_score: gameState.homeRuns,
-            away_score: gameState.awayRuns,
-            away_pitcher_id: gameState.awayPitcher.id,
-            home_pitcher_id: gameState.homePitcher.id,
-            away_team_lineup_ids: gameState.awayTeamLineup.map(atl => atl.id),
-            home_team_lineup_ids: gameState.homeTeamLineup.map(htl => htl.id),
-            away_team_is_batting: gameState.awayTeamBatting,
-            inning: gameState.inning,
-            number_of_outs: gameState.numberOfOuts,
-            current_away_team_batter_index: gameState.currAwayTeamBatter,
-            current_home_team_batter_index: gameState.currHomeTeamBatter
-        })
-        .eq('id', gameState.gameId)
-        .then(({ error }) => console.log('logs update error:', error));
-    })
+            .from('games')
+            .update({ 
+                home_score: gameState.homeRuns,
+                away_score: gameState.awayRuns,
+                away_pitcher_id: gameState.awayPitcher.id,
+                home_pitcher_id: gameState.homePitcher.id,
+                away_team_lineup_ids: gameState.awayTeamLineup.map(atl => atl.id),
+                home_team_lineup_ids: gameState.homeTeamLineup.map(htl => htl.id),
+                away_team_is_batting: gameState.awayTeamBatting,
+                inning: gameState.inning,
+                number_of_outs: gameState.numberOfOuts,
+                current_away_team_batter_index: gameState.currAwayTeamBatter,
+                current_home_team_batter_index: gameState.currHomeTeamBatter
+            })
+            .eq('id', gameState.gameId)
+            .then(({ error }) => console.log('logs update error:', error));
+    }, [gameState]);
 
     if (gameState) {
         return <GameLogger
